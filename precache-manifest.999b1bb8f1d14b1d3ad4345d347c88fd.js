79e99bd6e4d85a7d72bf6af48ca1a8006844df57
Evan <devillordking@gmail.com>
2018-11-23 00:31:17 +1100

    Updates

---
+++ precache-manifest.999b1bb8f1d14b1d3ad4345d347c88fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e7da2aaa1e9fa18b51f7b751f03ae0",
+    "url": "/scan-cashback-web/static/media/home-icon-selfie.69e7da2a.png"
+  },
+  {
+    "revision": "8b6b11a5969024c65ebb8f2b16267cf7",
+    "url": "/scan-cashback-web/static/media/home-icon-camera.8b6b11a5.png"
+  },
+  {
+    "revision": "ceb1dc753c1f4249d30a419b7d269d8e",
+    "url": "/scan-cashback-web/static/media/home-background.ceb1dc75.png"
+  },
+  {
+    "revision": "24da7e6a4a6735665ed6",
+    "url": "/scan-cashback-web/static/js/runtime~main.24da7e6a.js"
+  },
+  {
+    "revision": "940ee72e1571c002b175",
+    "url": "/scan-cashback-web/static/js/main.940ee72e.chunk.js"
+  },
+  {
+    "revision": "b5078b9d6b2e9df6e447",
+    "url": "/scan-cashback-web/static/js/1.b5078b9d.chunk.js"
+  },
+  {
+    "revision": "940ee72e1571c002b175",
+    "url": "/scan-cashback-web/static/css/main.0ffde59c.chunk.css"
+  },
+  {
+    "revision": "96321f9804708609f2700439c441ed3f",
+    "url": "/scan-cashback-web/index.html"
+  }
+];
