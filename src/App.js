a36769767ee932abe925690c8eaccb0e6228249f
Evan <evan@pulseid.com>
2018-11-23 00:20:32 +1100

    add steps description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Image from "react-bootstrap/lib/Image";
 import Col from "react-bootstrap/lib/Col";
 import Row from "react-bootstrap/lib/Row";
-import background from './images/home_background.png'
+import background from './images/home-background.png'
+import icon_camera from './images/home-icon-camera.png'
+import icon_selfie from './images/home-icon-selfie.png'
+import icon_note from './images/home-icon-note.png'
 
 class App extends Component {
     render() {
@@ -12,6 +15,30 @@ class App extends Component {
             <Grid>
                 <Row>
                     <Image src={background} responsive={true}/>
+                </Row>
+                <Row>
+                    <Col xs={2} md={4} className="vcenter">
+                        <Image src={icon_camera} responsive={true}/>
+                    </Col>
+                    <Col xs={10} md={8} className="vcenter">
+                        <div className="home-sub">Upload a picture of your receipt</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2} md={4} className="vcenter">
+                        <Image src={icon_selfie} responsive={true}/>
+                    </Col>
+                    <Col xs={10} md={8} className="vcenter">
+                        <div className="home-sub">Take a picture of yourself at the store</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2} md={4} className="vcenter">
+                        <Image src={icon_note} responsive={true}/>
+                    </Col>
+                    <Col xs={10} md={8} className="vcenter">
+                        <div className="home-sub">Enter your details</div>
+                    </Col>
                 </Row>
             </Grid>
         );
