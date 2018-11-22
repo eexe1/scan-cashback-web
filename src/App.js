7c3e3572e816e7aacf407903fcdab50d5d47f932
Evan <evan@pulseid.com>
2018-11-22 23:20:02 +1100

    update gitignore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Grid from 'react-bootstrap/lib/Grid';
+import Image from "react-bootstrap/lib/Image";
+import Col from "react-bootstrap/lib/Col";
+import Row from "react-bootstrap/lib/Row";
+import background from './images/home_background.png'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Image src={background} responsive={true}/>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 export default App;
