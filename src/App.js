f057cc368821ed4f9440fe0b5a2ac54c9a7937c7
Evan <devillordking@gmail.com>
2018-11-24 16:35:00 +1100

    add form button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Image from "react-bootstrap/lib/Image";
 import Col from "react-bootstrap/lib/Col";
 import Row from "react-bootstrap/lib/Row";
+import Button from "react-bootstrap/lib/Button";
 import background from './images/home-background.png'
 import icon_camera from './images/home-icon-camera.png'
 import icon_selfie from './images/home-icon-selfie.png'
 import icon_note from './images/home-icon-note.png'
 
 class App extends Component {
+
+    static formUrl = 'https://docs.google.com/forms/d/198veljnTFB1s9Sgc583At2a48NPKA6etioHb-HmxQTg/edit?usp=sharing';
+
+    constructor(props, context) {
+        super(props, context);
+        App.handleSubmit = App.handleSubmit.bind(this);
+
+    }
+
+    static handleSubmit() {
+        window.open(App.formUrl, "_blank")
+    }
+
     render() {
         return (
             <Grid>
@@ -38,6 +52,20 @@ class App extends Component {
                     </Col>
                     <Col xs={10} md={8} className="vcenter">
                         <div className="home-sub">Enter your details</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={4} md={4} xsOffset={4} className="vcenter">
+                        <Button bsStyle="primary" bsSize="large"
+                                onClick={App.handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={12} className="vcenter">
+                        <p>*A maximum 5$ amazon giftcard delivered to your email address. </p>
                     </Col>
                 </Row>
             </Grid>
